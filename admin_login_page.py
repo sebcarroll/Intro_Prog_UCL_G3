0b9445f9a855e84818c1d5ef88c48fa67b77a541
CostasHadjineophytou <constantinos.hadjineophytou.23@ucl.ac.uk>
2023-11-18 23:24:11 +0000

    Admin Login Page
    Admin login logic complete - needs gui arrangement

--- admin_login_page.py
+++ admin_login_page.py
@@ -1,4 +1,16 @@
 import tkinter as tk
+from tkinter import messagebox
+
+class t_case_sensitive(Exception):
+    pass
+class t_no_text(Exception):
+    pass
+class t_incorrect_details(Exception):
+    pass
+class t_deleted_account(Exception):
+    pass
+class t_deactivated_account(Exception):
+    pass
 
 class AdminLoginPage(tk.Frame):
     def __init__(self, root, go_to_admin_main):
@@ -8,22 +20,83 @@ class AdminLoginPage(tk.Frame):
 
         welcome_label = tk.Label(
             self,
-            text='Welcome to the UCL Humanity Rescue Portal',
+            text='Welcome to the UCL Humanity Rescue Admin Portal',
             font=('TkDefaultFont', 25, 'bold'),
             foreground='orange'
         )
-        welcome_label.pack(padx=50, pady=50)
+        welcome_label.grid(row=1, column=0)
 
         instruction_label = tk.Label(
             self,
-            text='Please select admin or volunteer sign in',
+            text='Please sign in',
             font=('TkDefaultFont', 20)
         )
-        instruction_label.pack(pady=25)
+        instruction_label.grid(row=2, column=0)
 
-        login_btn = tk.Button(self, text="Admin Login", command=self.on_login)
-        login_btn.pack(ipadx=50, ipady=15)
+        # self.t_volunteer_login
+        # self.t_details_confirmation
 
+        # Volunteer frame put on screen
+        # self.t_volunteer_frame = tk.Frame(self)
+        # self.t_volunteer_frame.pack()
+
+        # Volunteer frame white boarder
+        # self.t_info_frame = tk.LabelFrame(self.t_volunteer_frame, text='Volunteer login', font=25)
+        # self.t_info_frame.grid(row=0, column=0, padx=20, pady=10)
+
+        # Username entry box
+        self.name_label = tk.Label(self, text='Username')
+        self.name_label.grid(row=6, column=0)
+        self.name_entry = tk.Entry(self)
+        self.name_entry.grid(row=6, column=1)
+
+        # Password entry box
+        self.password_label2 = tk.Label(self, text='Password')
+        self.password_label2.grid(row=8, column=0, pady=5)
+        self.password_entry = tk.Entry(self, show='*')
+        self.password_entry.grid(row=8, column=1)
+        self.password_entry.bind("<KeyPress>", self.caps_lock_on)
+        self.password_entry.bind("<KeyRelease>", self.caps_lock_off)
+
+        # Login button
+        #self.entry_button = tk.Button(self, text='Login', command=self.details_confirmation, height=1, width=20)
+        #self.entry_button.grid(row=5, column=0, pady=20)
+
+        login_btn = tk.Button(self, text="Admin Login", command=self.details_confirmation)
+        login_btn.grid(row=10, column=1, pady=10)
+        # Swap button from  to command=self.on_login to bypass
+
+
+
+        # Shows caps lock on/off (OLD BINDING NOT WORKING)
+        self.label_caps = tk.Label(self, text='')
+        self.label_caps.grid(row=4, column=1)
+        # self.bind("<KeyPress>", self.caps_lock_on)
+        # self.bind("<KeyRelease>", self.caps_lock_off)
+
+        # For caps lock on/off
+    def caps_lock_on(self, event):
+        if event.keysym == 'Caps_Lock':
+            self.label_caps.config(text='Caps lock is on')
+
+    def caps_lock_off(self, event):
+        if event.keysym == 'Caps_Lock':
+            self.label_caps.config(text='Caps lock is off')
+
+        # Admin login checker
+    def details_confirmation(self):
+        username = self.name_entry.get()
+        password = self.password_entry.get()
+            # Incorrect details:
+        if username != "admin":
+            tk.messagebox.showinfo(title='Invalid username', message='That is not a valid admin username')
+        elif username == "admin" and password != "111":
+            tk.messagebox.showinfo(title='Invalid password', message='That is not the admin password')
+        elif username == "admin" and password == "111":
+            # Correct details:
+            self.go_to_admin_main()
+
+    # use on_login to bypass login to admin_main:
     def on_login(self):
         # Add login logic here
         self.go_to_admin_main()
