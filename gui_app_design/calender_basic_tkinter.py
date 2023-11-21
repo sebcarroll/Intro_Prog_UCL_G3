5f9d28ab7215fb5a1f89300983fdb62efeb925c2
aa300503 <149485481+aa300503@users.noreply.github.com>
2023-11-21 18:35:49 +0000

    na calender_basic_tkinter.py
    exception handled calendar

--- gui_app_design/calender_basic_tkinter.py
+++ gui_app_design/calender_basic_tkinter.py
@@ -1,40 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from datetime import datetime
+
+def is_valid_date(day, month, year):
+    try:
+        date_str = f"{day} {month} {year}"
+        selected_date= datetime.strptime(date_str, "%d %B %Y").date()
+        current_date = datetime.now().date()
+        return selected_date >= current_date
+        return True
+    except ValueError:
+        return False
+
 
 def get_selected_date():
     day = day_combobox.get()
     month = month_combobox.get()
     year = year_combobox.get()
-    selected_date = f"{day} {month} {year}"
-    messagebox.showinfo("Selected Date", f"You selected: {selected_date}")
 
-root = tk.Tk()
-root.title("Custom Date Input Example")
+    if is_valid_date(day, month, year):
+        selected_date = f"{day} {month} {year}"
+        messagebox.showinfo("Selected Date", f"start date of event: {selected_date}")
+    else:
+        messagebox.showerror("Invalid Date", "Please select a valid date.")
 
-# Days
-day_label = ttk.Label(root, text="Day:")
-day_label.grid(row=0, column=0)
-day_combobox = ttk.Combobox(root, values=list(range(1, 32)))
-day_combobox.grid(row=0, column=1)
-day_combobox.set(1)  # Default to the 1st day
+nawindow = tk.Tk()
+nawindow.title("Calendar Humanitarian Crisis")
 
-# Months
-month_label = ttk.Label(root, text="Month:")
-month_label.grid(row=0, column=2)
-month_combobox = ttk.Combobox(root, values=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
-month_combobox.grid(row=0, column=3)
-month_combobox.set("January")  # Default to January
 
-# Years
-year_label = ttk.Label(root, text="Year:")
-year_label.grid(row=0, column=4)
-year_combobox = ttk.Combobox(root, values=list(range(2020, 2025)))
-year_combobox.grid(row=0, column=5)
-year_combobox.set(2023)  # Default to the current year
-
-# Button to get the selected date
-get_date_button = ttk.Button(root, text="Get Selected Date", command=get_selected_date)
-get_date_button.grid(row=1, column=0, columnspan=6, pady=10)
-
-root.mainloop()
+current_date = datetime.now()
+current_year = current_date.year
+current_month = current_date.strftime("%B")
+current_day = current_date.day
