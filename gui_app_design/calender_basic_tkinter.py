dbff83f03793c6666f7eccea77a1da15929a3cb9
aa300503 <149485481+aa300503@users.noreply.github.com>
2023-11-21 18:44:44 +0000

    Update calender_basic_tkinter.py
    whoel thing

--- gui_app_design/calender_basic_tkinter.py
+++ gui_app_design/calender_basic_tkinter.py
@@ -33,3 +33,36 @@ current_date = datetime.now()
 current_year = current_date.year
 current_month = current_date.strftime("%B")
 current_day = current_date.day
+
+
+day_label = ttk.Label(nawindow, text="Day:")
+day_label.grid(row=0, column=0)
+day_combobox = ttk.Combobox(nawindow, values=list(range(1, 32)))
+day_combobox.grid(row=0, column=1)
+day_combobox.set(current_day)
+
+
+month_label = ttk.Label(nawindow, text="Month:")
+month_label.grid(row=0, column=2)
+month_combobox = ttk.Combobox(nawindow, values=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+month_combobox.grid(row=0, column=3)
+month_combobox.set(current_month)
+
+
+year_label = ttk.Label(nawindow, text="Year:")
+year_label.grid(row=0, column=4)
+year_combobox = ttk.Combobox(nawindow, values=list(range(current_year, current_year + 5)))
+year_combobox.grid(row=0, column=5)
+year_combobox.set(current_year)
+
+
+get_date_button = ttk.Button(nawindow, text="select date", command=get_selected_date)
+get_date_button.grid(row=1, column=0, columnspan=6, pady=10)
+
+nawindow.mainloop()
+
+
+current_date = datetime.now()
+current_year = current_date.year
+current_month = current_date.strftime("%B")
+current_day = current_date.day
