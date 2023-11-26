a61b867c826f95f151ec162633a0612525bab674
Sebastian Carroll <67208864+sebcarroll@users.noreply.github.com>
2023-11-26 11:31:54 +0000

    Minor bug fixes to resource_allocation_csv_creation - should now not experience any issues at all

--- resource_allocation_csv_creation.py
+++ resource_allocation_csv_creation.py
@@ -1,6 +1,7 @@
 import csv
 import os
 
+data_to_add = [1, 10, 10, 100, 100, 7, 7, 10]
 def save_information_csv(data_to_add):
     file_name = 'camp_information.csv'
     columns = ['camp_id', 'no_refugees', 'no_weeks_aid', 'total_food_supplied', 'total_medicine_supplied',
@@ -10,11 +11,13 @@ def save_information_csv(data_to_add):
         with open(file_name, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(columns)
-            writer.writerows(data_to_add)
+            writer.writerow(data_to_add)
     else:
 
         with open(file_name, mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            writer.writerows(data_to_add)
+            writer.writerow(data_to_add)
 
     print(f"Data added to {file_name}.")
+
+save_information_csv(data_to_add)
