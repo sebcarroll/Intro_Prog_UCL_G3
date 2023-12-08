623034f50df27350c23e7990ec4b638d0759c3bf
yingzhe-f <141032483+yingzhe-f@users.noreply.github.com>
2023-12-08 16:45:10 +0000

    complete edit_camp_details.py

--- VolunteerSubpages/resource_display.py
+++ VolunteerSubpages/resource_display.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+import pandas as pd
 
 def resource_display(window, back_button_to_volunteer_main):
     for i in window.winfo_children():
@@ -6,14 +8,57 @@ def resource_display(window, back_button_to_volunteer_main):
     resources_frame = tk.Frame(window)
     resources_frame.grid()
 
+    crisis_df = pd.read_csv('crisis_events.csv')
+
+    resources_df = crisis_df.loc[:, ['New Camp ID', 'Total Meals', 'Total Medicine']]
+    resources_df = resources_df.fillna(0)
+    columns = ['New Camp ID', 'Total Meals', 'Total Medicine']
+    total_meals = 100000
+    total_medicine = 1000000
+    meals_used = resources_df['Total Meals'].sum()
+    medicine_used = resources_df['Total Medicine'].sum()
+    leftover_meals = total_meals - meals_used
+    leftover_medicine = total_medicine - medicine_used
+
+    # GIVES THE LEFT FRAME
+    availability_frame = tk.Frame(resources_frame, width=150, height=400)
+    availability_frame.grid(row=4, column=0, padx=10, pady=5)
+
+    available_meals = tk.Label(availability_frame, text='Meals Left:', font=('Helvetica', 12))
+    available_meals.grid(row=0, column=0)
+
+    available_meals_number = tk.Label(availability_frame, text=f"{leftover_meals}", font=('Helvetica', 12))
+    available_meals_number.grid(row=0, column=20)
+
+    available_meds = tk.Label(availability_frame, text=f"Medicine Left", font=('Helvetica', 12))
+    available_meds.grid(row=5, column=0)
+
+    available_meals_number = tk.Label(availability_frame, text=f"{leftover_medicine}", font=('Helvetica', 12))
+    available_meals_number.grid(row=5, column=20)
+
+    # GIVES THE RIGHT FRAME
+    resources_df_frame = tk.Frame(resources_frame, width=150, height=400)
+    resources_df_frame.grid(row=4, column=5, padx=10, pady=5)
+
+    tree = ttk.Treeview(resources_df_frame, columns=columns, show='headings')
+
+    tree.heading('New Camp ID', text='Camp ID')
+    tree.heading('Total Meals', text='Meals')
+    tree.heading('Total Medicine', text='Medicine')
+
+    for _, row in resources_df.iterrows():
+        tree.insert('', tk.END, values=row.tolist())
+
+    scrollbar = ttk.Scrollbar(resources_df_frame, orient=tk.VERTICAL, command=tree.yview)
+    tree.configure(yscrollcommand=scrollbar.set)
+    scrollbar.grid(row=0, column=2, sticky='ns')
+    tree.grid(row=0, column=0, sticky='nsew')
+
     # Title
-    resources_title = tk.Label(resources_frame, text='Resources Currently Available', font=('tkDefault', 30))
-    resources_title.grid(row=0, column=1)
+    resources_title = tk.Label(resources_frame, text='Allocated Resources', font=('TkDefaultFont', 20))
+    resources_title.grid(row=0, column=5, columnspan=2)
 
-    # Creating label frame
-    resources_labelframe = tk.LabelFrame(resources_frame)
-    resources_labelframe.grid(row=1, column=1)
 
-    # Not sure what is meant to go in here at the moment so will just leave it for now.
     t_back_button = tk.Button(resources_frame, text='Back to Home', command=back_button_to_volunteer_main)
-    t_back_button.grid(row=9, column=0, padx=5, pady=10)
+    t_back_button.grid(row=20, column=0, padx=5, pady=10, columnspan=2)
+
