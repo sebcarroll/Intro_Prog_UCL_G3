2f1f1fa76f14a1b29145585fb855e0ecf3b441df
TomWHogg <148106713+TomWHogg@users.noreply.github.com>
2023-12-12 12:19:55 +0000

    Updated UI

--- VolunteerSubpages/edit_personal_information.py
+++ VolunteerSubpages/edit_personal_information.py
@@ -21,94 +21,173 @@ def edit_personal_info(window, username, y_personal_info, t_personal_information
     t_personal_frame.grid()
 
     t_personal_labelframe = tk.LabelFrame(t_personal_frame)
-    t_personal_labelframe.grid(row=3, column=3, padx=(window.winfo_width() - t_personal_frame.winfo_reqwidth()) // 3)
+    t_personal_labelframe.grid(row=1, column=0, padx=(window.winfo_width() - t_personal_frame.winfo_reqwidth()) // 4)
 
     personal_title = tk.Label(t_personal_frame, text='Edit details', font=('Arial Bold', 30))
-    personal_title.grid(row=0, column=3, pady=30)
+    personal_title.grid(row=0, column=0, pady=30)
 
     #  Currently set name and current title
 
     current_title = tk.Label(t_personal_labelframe, text='Current details', font=('tkDefault', 20))
-    current_title.grid(row=4, column=2)
+    current_title.grid(row=0, column=1, padx=5, pady=5)
 
 
 
     # Edit column title
     edit_title = tk.Label(t_personal_labelframe, text='Edit', font=('tkDefault', 20))
-    edit_title.grid(row=4, column=3, padx=5)
+    edit_title.grid(row=0, column=2, padx=5, pady=5)
 
     # Name entry box
     name_label = tk.Label(t_personal_labelframe, text='Name: ', font=('tkDefault', 15))
-    name_label.grid(row=5, column=1)
+    name_label.grid(row=1, column=0, padx=5, pady=5)
     preset_name = tk.Label(t_personal_labelframe, text=y_personal_info[username]['name'],
                            font=('tkDefault', 15))
-    preset_name.grid(row=5, column=2, padx=5)
+    preset_name.grid(row=1, column=1, padx=5, pady=5)
     t_personal_nameEntry = tk.Entry(t_personal_labelframe,)
-    t_personal_nameEntry.grid(row=5, column=3)
+    t_personal_nameEntry.grid(row=1, column=2, padx=5, pady=5, sticky='ew')
 
     # Email header/label
     # t_personal_email = tk.Label(t_personal_labelframe, text=' Edit Email address')
     # Current email
     email_label = tk.Label(t_personal_labelframe, text='Email address: ', font=('tkDefault', 15))
-    email_label.grid(row=7, column= 1, padx= 5)
+    email_label.grid(row=2, column= 0, padx=5, pady=5)
     personal_email = tk.Label(t_personal_labelframe,
                                    text=y_personal_info[username]['Email Address'],
                                    font=('tkDefault', 15))
-    personal_email.grid(row=7, column=2, padx=5)
+    personal_email.grid(row=2, column=1, padx=5, pady=5)
 
     # Email entry box
     t_personal_emailEntry = tk.Entry(t_personal_labelframe)
-    t_personal_emailEntry.grid(row=7, column=3)
+    t_personal_emailEntry.grid(row=2, column=2, padx=5, pady=5, sticky='ew')
     # t_phonenumber = tk.Label(t_personal_labelframe, text='Edit Phone number')
 
     # Phone number Entry and label
     phone_label = tk.Label(t_personal_labelframe, text='Phone number: ', font=('tkDefault', 15))
-    phone_label.grid(row=9, column=1, padx=5)
+    phone_label.grid(row=3, column=0, padx=5, pady=5)
     phone_number = tk.Label(t_personal_labelframe,
                                  text=y_personal_info[username]['Phone Number'],
                                  font=('tkDefault', 15))
-    phone_number.grid(row=9, column=2, padx=5)
+    phone_number.grid(row=3, column=1, padx=5, pady=5)
     t_phonenumberEntry = tk.Entry(t_personal_labelframe)
-    t_phonenumberEntry.grid(row=9, column=3)
+    t_phonenumberEntry.grid(row=3, column=2, padx=5, pady=5, sticky='ew')
 
     # Commitment label
     commitment_type = tk.Label(t_personal_labelframe, text='Commitment type: ', font=('tkDefault', 15))
-    commitment_type.grid(row=11, column=1, padx= 5)
+    commitment_type.grid(row=4, column=0, padx=5, pady=5)
     commitment_label = tk.Label(t_personal_labelframe,
                                      text=y_personal_info[username]['Commitment'],
                                      font=('tkDefault', 15))
-    commitment_label.grid(row=11, column=2, padx=5)
+    commitment_label.grid(row=4, column=1, padx=5, pady=5)
 
     # Commitment type entry
     t_commitmentEntry = ttk.Combobox(t_personal_labelframe,
                                           values=['Full time', 'Part time', 'Occasional'],
                                           state='readonly')
-    t_commitmentEntry.grid(row=11, column=3)
+    t_commitmentEntry.grid(row=4, column=2, padx=5, pady=5, sticky='ew')
 
     # Work type label
     work_type = tk.Label(t_personal_labelframe, text='Work type: ', font=('tkDefault', 15))
-    work_type.grid(row=13, column=1, padx=5)
+    work_type.grid(row=5, column=0, padx=5, pady=5)
     work_type_label = tk.Label(t_personal_labelframe,
                                     text=y_personal_info[username]['Work Type'],
                                     font=('tkDefault', 15))
-    work_type_label.grid(row=13, column=2, padx=5)
+    work_type_label.grid(row=5, column=1, padx=5, pady=5)
 
     # Work type entry
     t_worktypeEntry = ttk.Combobox(t_personal_labelframe, values=['Medical Aid', 'Food counselling'])
-    t_worktypeEntry.grid(row=13, column=3)
+    t_worktypeEntry.grid(row=5, column=2, padx=5, pady=5, sticky='ew')
 
     # Store details box
-    t_store_details = tk.Button(t_personal_frame, text='Store details', command=lambda: [store_details_callback(t_personal_nameEntry, t_personal_emailEntry, t_phonenumberEntry, t_commitmentEntry, t_worktypeEntry), t_back_to_details],
+    t_store_details = tk.Button(t_personal_labelframe, text='Store details', command=lambda: [store_details_callback(t_personal_nameEntry, t_personal_emailEntry, t_phonenumberEntry, t_commitmentEntry, t_worktypeEntry), t_back_to_details],
                                      height=1, width=20)
-    t_store_details.grid(row=17, column=3)
+    t_store_details.grid(row=6, column=1, padx=5, pady=5)
 
     # Back to details page
-    t_back_to_details = tk.Button(t_personal_frame, text='Back to volunteer details', command=t_personal_information_base)
-    t_back_to_details.grid(row=18, column=3)
+    t_back_to_details = tk.Button(t_personal_labelframe, text='Back to volunteer details', command=t_personal_information_base)
+    t_back_to_details.grid(row=6, column=2, padx=5, pady=5)
 
-    back_to_summary = tk.Button(t_personal_frame, text='Back to Home',
+    back_to_summary = tk.Button(t_personal_labelframe, text='Back to Home',
                                      command=back_button_to_volunteer_main)
-    back_to_summary.grid(row=19, column=3)
+    back_to_summary.grid(row=6, column=0, padx=5, pady=5)
+
+    for i in range(7):
+        t_personal_frame.grid_rowconfigure(i, weight=1)
+    t_personal_frame.grid_columnconfigure(0, weight=1)
+
+
+    def new_refugee(window, y_camp_info, refugee_info, back_button_to_volunteer_main, store_details_callback):
+        for i in window.winfo_children():
+            i.grid_forget()
+        # Main frame for this whole page
+        refugeeframe = tk.Frame(window)
+        refugeeframe.grid()
+        crisis_df = pd.read_csv('crisis_events.csv')
+        camp_IDs = list(crisis_df['Camp ID'])
+
+        # Title for the page
+        refugee_title = tk.Label(refugeeframe, text='Create Refugee Profile', font=('TkinterDefault', 30))
+        refugee_title.grid(row=0, column=1, pady=30)
+
+        # Label frame for this page that then stores all of the labels and entries
+        refugee_labelframe = tk.LabelFrame(refugeeframe)
+        refugee_labelframe.grid(row=1, column=1)
+
+        # Camp ID
+        t_camp_ID_label = tk.Label(refugee_labelframe, text='Camp ID', font=('TkinterDefault', 15))
+        t_camp_ID_label.grid(row=3, column=3)
+        t_camp_IDbox = ttk.Combobox(refugee_labelframe, values=camp_IDs)
+        t_camp_IDbox.grid(row=3, column=4, padx=5)
+
+        # Refugee name
+        name_label = tk.Label(refugee_labelframe, text='Name', font=('TkinterDefault', 15))
+        name_label.grid(row=5, column=3, padx=5)
+        name_entry = tk.Entry(refugee_labelframe)
+        name_entry.grid(row=5, column=4, padx=5)
+
+        # Family members
+        family_label = tk.Label(refugee_labelframe, text='Enter total number of family members',
+                                font=('TkinterDefault', 15))
+        family_label.grid(row=7, column=3, padx=5)
+        family_labelbox = ttk.Spinbox(refugee_labelframe, from_=0, to=20, style='info.TSpinbox')
+        family_labelbox.grid(row=7, column=4, padx=5)
+
+        # Medical conditions, we need to add dictionaries and everything for this
+        medical_conditionslabel = tk.Label(refugee_labelframe,
+                                           text='Enter any medical condition(s) for each family member. If none, please enter \"none\"',
+                                           font=("TkinterDefault", 15))
+        medical_conditionslabel.grid(row=9, column=3, padx=5)
+        t_medical_conditionsEntry = tk.Entry(refugee_labelframe)
+        t_medical_conditionsEntry.grid(row=9, column=4, padx=5)
+
+        # Languages spoken by refugees
+        languages_spokenlabel = tk.Label(refugee_labelframe, text='Please select main language spoken in the family',
+                                         font=('TkinterDefault', 15))
+        languages_spokenlabel.grid(row=11, column=3, padx=5)
+        t_languages_spokenEntry = ttk.Combobox(refugee_labelframe,
+                                               values='English Chinese Hindi Spanish French Arabic Bengali Portuguese Russian Urdu Indonesian German Swahili Marathi Tamil Telugu Turkish Vietnamese Korean Italian Thai Gujarati Persian Polish Pashto Kannada Ukrainian Somali Kurdish')
+        t_languages_spokenEntry.grid(row=11, column=4, padx=5)
+
+        # secondlanguage entry box
+        second_languagelabel = tk.Label(refugee_labelframe,
+                                        text='Please enter any other languages spoken by each family member. If none, please enter \'none\'',
+                                        font=('TkinterDefault', 15))
+        second_languagelabel.grid(row=13, column=3, padx=5)
+        t_second_languageEntry = tk.Entry(refugee_labelframe)
+        t_second_languageEntry.grid(row=13, column=4, padx=5)
+
+        na_store_details = tk.Button(refugee_labelframe, text="Store refugee info", command=lambda: [
+            store_details_callback(t_camp_IDbox, family_labelbox, t_medical_conditionsEntry, t_languages_spokenEntry,
+                                   t_second_languageEntry, name_entry), back_button_to_volunteer_main], height=1,
+                                     width=20)
+        na_store_details.grid(row=15, column=3)
+
+        # Back button
+        t_back_button = tk.Button(refugee_labelframe, text='Back to Home', command=back_button_to_volunteer_main)
+        t_back_button.grid(row=17, column=3, padx=5, pady=10)
+
+        for i in range(11):
+            t_personal_frame.grid_rowconfigure(i, weight=1)
+        t_personal_frame.grid_columnconfigure(0, weight=1)
 
     return t_personal_nameEntry, t_personal_emailEntry,t_phonenumberEntry, t_commitmentEntry, t_worktypeEntry
 
--- VolunteerSubpages/personal_information.py
+++ VolunteerSubpages/personal_information.py
@@ -10,55 +10,59 @@ def personal_information(window, username, y_personal_info, t_personal_info_edit
     t_personal_frame.grid(padx=(window.winfo_width() - t_personal_frame.winfo_reqwidth()) // 3)
 
     t_personal_labelframe = tk.LabelFrame(t_personal_frame)
-    t_personal_labelframe.grid(row=3, column=0, padx=10, pady=30, columnspan=4)
+    t_personal_labelframe.grid(row=1, column=0, padx=10, pady=30, columnspan=4)
 
     t_personal_title = tk.Label(t_personal_frame, text='Volunteer Details', font=('Arial Bold', 30))
     t_personal_title.grid(row=0, column=0, columnspan=4, pady=30)
 
     # Name label
     t_personal_namelabel = tk.Label(t_personal_labelframe, text='Name', font=('TkDefaultFont', 15))
-    t_personal_namelabel.grid(row=4, column=0, pady=5)
+    t_personal_namelabel.grid(row=2, column=0, pady=5, padx=5)
     t_personal_name = tk.Label(t_personal_labelframe, text=y_personal_info[username]['name'],
                                     font=('TkDefaultFont', 15))
-    t_personal_name.grid(row=4, column=1, pady=5)
+    t_personal_name.grid(row=2, column=1, pady=5, padx=5)
 
     # Email label
     t_personal_emailabel = tk.Label(t_personal_labelframe, text='Email Address: ', font=('TkDefaultFont', 15))
-    t_personal_emailabel.grid(row=6, column=0, pady=5)
+    t_personal_emailabel.grid(row=3, column=0, pady=5, padx=5)
     t_personal_email = tk.Label(t_personal_labelframe,
                                      text=y_personal_info[username]['Email Address'],
                                      font=('TkDefaultFont', 15))
-    t_personal_email.grid(row=6, column=1, pady=5)
+    t_personal_email.grid(row=3, column=1, pady=5, padx=5)
 
     # Phone label
     t_personal_phonenumberlabel = tk.Label(t_personal_labelframe, text='Phone number: ',
                                                 font=('TkDefaultFont', 15))
-    t_personal_phonenumberlabel.grid(row=8, column=0, pady=5)
+    t_personal_phonenumberlabel.grid(row=4, column=0, pady=5, padx=5)
     t_phonenumber = tk.Label(t_personal_labelframe, text=y_personal_info[username]['Phone Number'],
                                   font=('TkDefaultFont', 15))
-    t_phonenumber.grid(row=8, column=1, pady=5)
+    t_phonenumber.grid(row=4, column=1, pady=5, padx=5)
 
     t_personal_commitmentlabel = tk.Label(t_personal_labelframe, text='Commitment: ',
                                                font=('TkDefaultFont', 15))
-    t_personal_commitmentlabel.grid(row=10, column=0, pady=5)
+    t_personal_commitmentlabel.grid(row=5, column=0, pady=5, padx=5)
     t_commitment = tk.Label(t_personal_labelframe, text=y_personal_info[username]['Commitment'],
                                  font=('TkDefaultFont', 15))
-    t_commitment.grid(row=10, column=1, pady=5)
+    t_commitment.grid(row=5, column=1, pady=5, padx=5)
 
     t_personal_namelabel = tk.Label(t_personal_labelframe, text='Work type: ', font=('TkDefaultFont', 15))
-    t_personal_namelabel.grid(row=12, column=0, pady=5)
+    t_personal_namelabel.grid(row=6, column=0, pady=5, padx=5)
 
     t_work_type_label = tk.Label(t_personal_labelframe, text=y_personal_info[username]['Work Type'],
                                       font=('TkDefaultFont', 15))
-    t_work_type_label.grid(row=12, column=1, pady=5)
+    t_work_type_label.grid(row=6, column=1, pady=5, padx=5)
 
     # Store details button
     t_edit_details = tk.Button(t_personal_frame, text='Edit details', command=t_personal_info_edit,
                                     height=1, width=20)
-    t_edit_details.grid(row=14, column=1, columnspan=2, pady=10, padx=10)
+    t_edit_details.grid(row=7, column=1, columnspan=2, pady=10, padx=10)
 
     back_to_summary = tk.Button(t_personal_frame, text='Back to Home', command=back_button_to_volunteer_main)
-    back_to_summary.grid(row=17, column=1, columnspan=2, pady=10, padx=10)
+    back_to_summary.grid(row=8, column=1, columnspan=2, pady=10, padx=10)
+
+    for i in range(9):
+        t_personal_frame.grid_rowconfigure(i, weight=1)
+    t_personal_frame.grid_columnconfigure(0, weight=1)
 
 
 
