48e3fd0b492a81ca892f8324156c60bd2afc81c9
nikhita-bhatt <144359593+nikhita-bhatt@users.noreply.github.com>
2023-12-14 17:04:00 +0000

    added 'generate charts' function under view summaries - pie chart and bar chart

--- AdminSubpages/view_summaries.py
+++ AdminSubpages/view_summaries.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import pandas as pd
 from tkinter import messagebox
 from general_functions import validate_data
+import csv
 
 
 class AdminViewSummaries:
@@ -53,9 +54,13 @@ class AdminViewSummaries:
         delete_btn = tk.Button(btn_frame, text="Delete Event", command=lambda: self.delete_csv_data_entry(self.end_plan_tree, csv_file))
         delete_btn.grid(row=0, column=2, padx=20, pady=(50,10))
 
+        # View data button
+        generate_data_btn = tk.Button(btn_frame, text="Generate Charts", command=lambda: self.generate_charts_window())
+        generate_data_btn.grid(row=0, column=3, padx=20, pady=(50,10))
+
         # Back button
         back_button = tk.Button(btn_frame, text='Back to Home', command=self.back_button_to_admin_main)
-        back_button.grid(row=2, column=1, padx=5, pady=40)
+        back_button.grid(row=2, column=1, columnspan=2, padx=5, pady=40)
 
 
         # CSV data
@@ -68,6 +73,153 @@ class AdminViewSummaries:
             messagebox.showwarning("No data found",
                                    "There is a problem accessing the database\n\nThe file may be missing or corrupted")
 
+    def generate_charts_window(self):
+        # Create a new window for the pie chart
+        generate_charts_window = tk.Toplevel(self.window)
+        generate_charts_window.title("View charts")
+        generate_charts_window.geometry("800x400")
+
+        # Call the method to create the pie chart in the new window
+        self.create_pie_chart_status(generate_charts_window)
+        self.create_bar_chart_crisis_type(generate_charts_window)
+
+    def create_pie_chart_status(self, window):
+        # Read data from CSV file
+        active_count = 0
+        inactive_count = 0
+
+        with open('crisis_events.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                status = row['Status']
+                if status.lower() == 'active':
+                    active_count += 1
+                elif status.lower() == 'inactive':
+                    inactive_count += 1
+
+        # Calculate percentage
+        total = active_count + inactive_count
+        active_percentage = (active_count / total) * 360
+        inactive_percentage = (inactive_count / total) * 360
+
+        # Create a pie chart using canvas
+        canvas = tk.Canvas(window, width=200, height=200)
+        # canvas.grid(row=17, column=0, padx=10, pady=5)
+        canvas.pack()
+
+        # Create a canvas for the pie chart labels
+        canvas_legend_pie = tk.Canvas(window, width=200, height=200)
+        canvas_legend_pie.place(x=50,y=75)
+
+        # Draw active slice
+        canvas.create_arc(50, 50, 150, 150, start=0, extent=active_percentage, fill='green', outline='white')
+        # Draw inactive slice
+        canvas.create_arc(50, 50, 150, 150, start=active_percentage, extent=inactive_percentage, fill='red',
+                          outline='white')
+
+        # Add legend (key)
+        legend_labels_crisis_status = ['Active Crisis', 'Inactive Crisis']
+        legend_colors_crisis_status = ['green', 'red']
+
+        for i, label in enumerate(legend_labels_crisis_status):
+            # Draw legend rectangle
+            canvas_legend_pie.create_rectangle(10, 10 + i * 20, 30, 30 + i * 20, fill=legend_colors_crisis_status[i], outline='white')
+            # Draw legend label
+            canvas_legend_pie.create_text(40, 20 + i * 20, text=label, anchor=tk.W, fill='black')
+
+    def create_bar_chart_crisis_type(self, window):
+        # Read data from CSV file
+        war_count = 0
+        environmental_count = 0
+        supply_shortage_count = 0
+        political_unrest_count = 0
+        displacement_count = 0
+        other_count = 0
+
+        with open('crisis_events.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                status = row['Crisis Type']
+                if status.lower() == 'war':
+                    war_count += 1
+                elif status.lower() == 'environmental':
+                    environmental_count += 1
+                elif status.lower() == 'political unrest':
+                    political_unrest_count += 1
+                elif status.lower() == 'displacement':
+                    displacement_count += 1
+                elif status.lower() == 'supply shortage':
+                    supply_shortage_count += 1
+                elif status.lower() == 'other':
+                    other_count += 1
+
+        # Calculate total count
+        total = war_count + environmental_count + supply_shortage_count + political_unrest_count + displacement_count + other_count
+
+
+        # # Calculate percentage
+        # total = war_count + environmental_count + supply_shortage_count + political_unrest_count + displacement_count + other_count
+        # war_percentage = (war_count / total) * 360
+        # environmental_percentage = (environmental_count / total) * 360
+        # supply_shortage_percentage = (supply_shortage_count / total) * 360
+        # political_unrest_percentage = (political_unrest_count / total) * 360
+        # displacement_percentage = (displacement_count / total) * 360
+        # other_percentage = (other_count / total) * 360
+
+        # Create a bar chart using canvas
+        canvas = tk.Canvas(window, width=400, height=200)
+        canvas.pack()
+
+        # Create a canvas for the bar chart labels
+        canvas_legend_bar = tk.Canvas(window, width=200, height=200)
+        canvas_legend_bar.place(x=50,y=225)
+
+        # Define bar width and gap
+        bar_width = 40
+        gap = 20
+
+        # Calculate the x-coordinates for each bar
+        x_war = 50
+        x_environmental = x_war + bar_width + gap
+        x_supply_shortage = x_environmental + bar_width + gap
+        x_political_unrest = x_supply_shortage + bar_width + gap
+        x_displacement = x_political_unrest + bar_width + gap
+        x_other = x_displacement + bar_width + gap
+
+        # Calculate the heights of each bar
+        height_factor = 100  # You can adjust this factor to control the height of the bars
+        height_war = war_count * height_factor / total
+        height_environmental = environmental_count * height_factor / total
+        height_supply_shortage = supply_shortage_count * height_factor / total
+        height_political_unrest = political_unrest_count * height_factor / total
+        height_displacement = displacement_count * height_factor / total
+        height_other = other_count * height_factor / total
+
+        # Draw rectangles for each bar
+        canvas.create_rectangle(x_war, 150 - height_war, x_war + bar_width, 150, fill='red', outline='white')
+        canvas.create_rectangle(x_environmental, 150 - height_environmental, x_environmental + bar_width, 150, fill='blue', outline='white')
+        canvas.create_rectangle(x_supply_shortage, 150 - height_supply_shortage, x_supply_shortage + bar_width, 150, fill='green', outline='white')
+        canvas.create_rectangle(x_political_unrest, 150 - height_political_unrest, x_political_unrest + bar_width, 150, fill='yellow', outline='white')
+        canvas.create_rectangle(x_displacement, 150 - height_displacement, x_displacement + bar_width, 150, fill='purple', outline='white')
+        canvas.create_rectangle(x_other, 150 - height_other, x_other + bar_width, 150, fill='orange', outline='white')
+
+        # # Draw slices of the pie chart
+        # canvas.create_arc(50, 50, 150, 150, start=0, extent=war_percentage, fill='red', outline='white')
+        # canvas.create_arc(50, 50, 150, 150, start=war_percentage, extent=environmental_percentage, fill='blue', outline='white')
+        # canvas.create_arc(50, 50, 150, 150, start=war_percentage + environmental_percentage, extent=supply_shortage_percentage, fill='green', outline='white')
+        # canvas.create_arc(50, 50, 150, 150, start=war_percentage + environmental_percentage + supply_shortage_percentage, extent=political_unrest_percentage, fill='yellow', outline='white')
+        # canvas.create_arc(50, 50, 150, 150, start=war_percentage + environmental_percentage + supply_shortage_percentage + political_unrest_percentage, extent=displacement_percentage, fill='purple', outline='white')
+        # canvas.create_arc(50, 50, 150, 150, start=war_percentage + environmental_percentage + supply_shortage_percentage + political_unrest_percentage + displacement_percentage, extent=other_percentage, fill='orange', outline='white')
+
+        # Add legend (key)
+        legend_labels_crisis_type = ['War', 'Environmental', 'Supply Shortage', 'Political Unrest', 'Displacement', 'Other']
+        legend_colors_crisis_type = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+
+        for i, label in enumerate(legend_labels_crisis_type):
+            # Draw legend rectangle
+            canvas_legend_bar.create_rectangle(10, 10 + i * 20, 30, 30 + i * 20, fill=legend_colors_crisis_type[i], outline='white')
+            # Draw legend label
+            canvas_legend_bar.create_text(40, 20 + i * 20, text=label, anchor=tk.W, fill='black')
 
     def upload_csv_data(self, tree, filename):
         data = pd.read_csv(filename)
