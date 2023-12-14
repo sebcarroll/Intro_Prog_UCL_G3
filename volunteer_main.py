a05f4643e07eccb70dd5b0901826c7fa21e48726
nikhita-bhatt <144359593+nikhita-bhatt@users.noreply.github.com>
2023-12-14 18:31:21 +0000

    added feature to display camp ID for the volunteer logged in on their homepage

--- volunteer_main.py
+++ volunteer_main.py
@@ -112,7 +112,7 @@ class VolunteerHomepage():
         self.t_summary_title.grid(row=0, column=0, columnspan=2, sticky='news', padx=20, pady=10)
         self.t_summary_title.configure(background='grey')
 
-        self.show_camp_id_label = tk.Label(self.window, text=f"Your camp id: {self.camp_id}", font=("Arial", 20), fg="black")
+        self.show_camp_id_label = tk.Label(self.window, text=f"Your Camp ID: {self.camp_id}", font=("Arial", 20), fg="black")
         self.show_camp_id_label.place(x=550,y=75)
 
         self.t_summary_editdetails = tk.Button(self.window, text='Personal information',
