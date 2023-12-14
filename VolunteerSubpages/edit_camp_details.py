284390aaf9b660d5734d71f7d7ce70a4c7dd1ca1
CostasHadjineophytou <constantinos.hadjineophytou.23@ucl.ac.uk>
2023-12-14 23:27:42 +0000

    Volunteer can change camp ID now
    Still need to update the ID if changed in the variable from log in

--- VolunteerSubpages/edit_camp_details.py
+++ VolunteerSubpages/edit_camp_details.py
@@ -8,6 +8,7 @@ class none_selected(Exception):
     pass
 def edit_camp_details(window, y_camp_info, camp_id, back_button_to_volunteer_main):
     try:
+        print(camp_id)
         for i in window.winfo_children():
             i.grid_forget()
 
@@ -17,26 +18,22 @@ def edit_camp_details(window, y_camp_info, camp_id, back_button_to_volunteer_mai
         # Access the Camp_ID's
         crisis_df = pd.read_csv("crisis_events.csv", header=0)
         crisis_df = crisis_df.fillna(0)
-        camp_ID_choices = crisis_df.iloc[:, 0]
+        camp_ID_choices = crisis_df['Camp ID'].tolist()
 
         t_select_camp_title = tk.Label(t_edit_campframe, text='Select camp ID', font=('Arial Bold', 30), pady=30)
         t_select_camp_title.grid(row=0, column=1)
 
         camp_ID_listbox = tk.Listbox(t_edit_campframe, selectmode=tk.SINGLE)
-
-        for i in range(len(camp_ID_choices)):
-            if camp_ID_choices[i] == camp_id:
-                camp_ID_listbox.insert(tk.END, camp_ID_choices[i])
-            #else:
-                #camp_ID_listbox.insert(tk.END, "No camp assigned")
+        if camp_id in camp_ID_choices:
+            camp_ID_listbox.insert(tk.END, camp_id)
+        else:
+            camp_ID_listbox.insert(tk.END, "No camp assigned")
         camp_ID_listbox.grid(row=1, column=1, padx=5, pady=5)
 
         # BUTTONS:
-        # Save changes button, need to add this to a dictionary.
         t_save_changes = tk.Button(t_edit_campframe, text='Add/Remove Refugees', command=lambda: edit_refugee_no(crisis_df, camp_ID_listbox.get(tk.ACTIVE)))
         t_save_changes.grid(row=7, column=1, padx=5, pady=10)
 
-        # Back button
         t_back_button = tk.Button(t_edit_campframe, text='Back to Home', command=back_button_to_volunteer_main)
         t_back_button.grid(row=7, column=0, padx=5, pady=10)
 
@@ -50,55 +47,61 @@ def edit_camp_details(window, y_camp_info, camp_id, back_button_to_volunteer_mai
 
 def edit_refugee_no(df, selected_camp_id):
     try:
-        if selected_camp_id:
-            current_capacity = df.loc[df["Camp ID"] == selected_camp_id, 'Refugees'].values[0]
-            new_capacity = simpledialog.askinteger("Add/Remove refugees", f"Current total Capacity for Camp ID {selected_camp_id}: {current_capacity}"
-                                                                          f"\nHow many refugees would you like to add/remove?", initialvalue=1)
+        if selected_camp_id and selected_camp_id.isdigit():
+            current_capacity = df.loc[df["Camp ID"] == int(selected_camp_id), 'Refugees'].values[0]
+            new_capacity = simpledialog.askinteger("Add/Remove refugees",
+                                                   f"Current total Capacity for Camp ID {selected_camp_id}: {current_capacity}"
+                                                   f"\nHow many refugees would you like to add/remove?",
+                                                   initialvalue=0)
 
-
-            if type(new_capacity) == int:
-                # Update the DataFrame with the new value
-                df.loc[df["Camp ID"] == selected_camp_id, 'Capacity'] += new_capacity
+            if isinstance(new_capacity, int):
+                df.loc[df["Camp ID"] == int(selected_camp_id), 'Refugees'] += new_capacity
                 df.to_csv('crisis_events.csv', index=False)
-
-
             else:
                 raise ValueError
         else:
             raise none_selected
     except none_selected:
         messagebox.showwarning(title="Camp ID not selected", message="Please select a camp ID")
+    except ValueError:
+        messagebox.showwarning(title="Invalid Entry", message="Please enter a valid number")
 
 
 def edit_camp_id(df, selected_camp_id, listbox):
+    # Convert selected_camp_id to string if it's not
+    selected_camp_id_str = str(selected_camp_id)
     try:
-        if selected_camp_id:
-            # Ask the user for a new camp ID
-            new_id = simpledialog.askstring("Change camp ID", prompt="Enter new camp ID - using only numbers (5 characters ONLY)")
+        if selected_camp_id_str and selected_camp_id_str.isdigit():
+            new_id = simpledialog.askstring("Change camp ID", "Enter new camp ID - using only numbers (5 characters ONLY)")
 
-            # Check if the new ID is not empty and is different from the current ID
-            if new_id.isnumeric() and len(new_id) != 5:
-                # Check if the new ID already exists in the DataFrame
-                if new_id not in df['Camp ID'].values:
-                    # Update the camp ID in the DataFrame
-                    df.loc[df['Camp ID'] == selected_camp_id, 'Camp ID'] = new_id
-
-                    # Convert the 'New Camp ID' column to a list
-                    new_camp_ids = df['Camp ID'].tolist()
-
-                    # Clear the listbox and insert the updated camp IDs
+            if new_id and new_id.isnumeric() and len(new_id) == 5:
+                if int(new_id) not in df['Camp ID'].values:
+                    df.loc[df['Camp ID'] == int(selected_camp_id_str), 'Camp ID'] = int(new_id)
                     listbox.delete(0, tk.END)
-                    for c_id in new_camp_ids:
-                        listbox.insert(tk.END, c_id)
-
-                    # Save the DataFrame to the CSV file
+                    listbox.insert(tk.END, new_id)
                     df.to_csv('crisis_events.csv', index=False)
+
+                    # Update Camp ID in other CSV files
+                    update_camp_id_in_other_csvs(int(selected_camp_id_str), int(new_id))
                 else:
                     raise taken_ID
             else:
                 raise ValueError
-
+        else:
+            raise none_selected
     except taken_ID:
         messagebox.showwarning("Error", "ID is already taken")
     except ValueError:
-        messagebox.showwarning("Invalid ID", message="Please enter a valid ID")
+        messagebox.showwarning("Invalid ID", "Please enter a valid ID")
+    except none_selected:
+        messagebox.showwarning("Camp ID not selected", "Please select a camp ID")
+
+def update_camp_id_in_other_csvs(old_id, new_id):
+    for filename in ["volunteer_info.csv", "refugee_info.csv"]:
+        try:
+            df = pd.read_csv(filename)
+            if 'Camp ID' in df.columns:
+                df['Camp ID'] = df['Camp ID'].replace(old_id, new_id)
+                df.to_csv(filename, index=False)
+        except Exception as e:
+            messagebox.showwarning("Error", f"Error updating {filename}: {e}")
