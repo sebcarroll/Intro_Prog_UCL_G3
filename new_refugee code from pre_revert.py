46e999c4bcc1ebdeead94e12ee332c9e96fa630f
Sebastian Carroll <67208864+sebcarroll@users.noreply.github.com>
2023-12-14 21:21:28 +0000

    Prerevert code being saved here

---
+++ new_refugee code from pre_revert.py
@@ -0,0 +1,164 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import pandas as pd
+import csv
+
+def new_refugee(window, y_camp_info, refugee_info, back_button_to_volunteer_main, store_details_callback):
+    for i in window.winfo_children():
+        i.grid_forget()
+    # Main frame for this whole page
+    refugeeframe = tk.Frame(window)
+    refugeeframe.grid()
+    crisis_df = pd.read_csv('crisis_events.csv')
+    active_camps= crisis_df[crisis_df['Status']== 'Active']
+    camp_IDs = list(active_camps['Camp ID'])
+
+    camp_ids_from_csv = []
+    try:
+        with open('crisis_events.csv', 'r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+            for row in csv_reader:
+                if row[7] == "Active":
+                    camp_ids_from_csv.append(row[0])
+    except FileNotFoundError:
+        print("Error: 'crisis_events.csv' file not found.")
+
+    camp_IDs_unfiltered = list(camp_ids_from_csv)
+    camp_IDs_filtered = []
+    for camp_ids in camp_IDs_unfiltered:
+        from volunteer_login_page import volunteer_camp_id_for_new_refugee
+        print(volunteer_camp_id_for_new_refugee)
+        if int(float(camp_ids)) == int(float(volunteer_camp_id_for_new_refugee)):
+            camp_IDs_filtered.append(camp_ids)
+
+    camp_IDs = camp_IDs_filtered
+
+    # Title for the page
+    refugee_title = tk.Label(refugeeframe, text='Create Refugee Profile', font=('TkinterDefault', 30))
+    refugee_title.grid(row=0, column=0, pady=30)
+
+    # Label frame for this page that then stores all of the labels and entries
+    refugee_labelframe = tk.LabelFrame(refugeeframe)
+    refugee_labelframe.grid(row=1, column=0)
+
+    # Camp ID
+    t_camp_ID_label = tk.Label(refugee_labelframe, text='Camp ID', font=('TkinterDefault', 15))
+    t_camp_ID_label.grid(row=2, column=0)
+    t_camp_IDbox = ttk.Combobox(refugee_labelframe, values=camp_IDs)
+    t_camp_IDbox.grid(row=2, column=1, padx=5, pady=5)
+
+    # Refugee name
+    name_label = tk.Label(refugee_labelframe, text='Name', font=('TkinterDefault', 15))
+    name_label.grid(row=3, column=0, padx=5, pady=5)
+    name_entry = tk.Entry(refugee_labelframe)
+    name_entry.grid(row=3, column=1, padx=5, pady=5)
+
+    # Family members
+    family_label = tk.Label(refugee_labelframe, text='Enter total number of family members',
+                            font=('TkinterDefault', 15))
+    family_label.grid(row=4, column=0, padx=5, pady=5)
+    family_labelbox = ttk.Spinbox(refugee_labelframe, from_=0, to=20, style='info.TSpinbox')
+    family_labelbox.grid(row=4, column=1, padx=5, pady=5)
+
+    # Medical conditions, we need to add dictionaries and everything for this
+    medical_conditionslabel = tk.Label(refugee_labelframe,
+                                       text='Enter any medical condition(s) for each family member. If none, please enter \"none\"',
+                                       font=("TkinterDefault", 15))
+    medical_conditionslabel.grid(row=5, column=0, padx=5, pady=5)
+    t_medical_conditionsEntry = tk.Entry(refugee_labelframe)
+    t_medical_conditionsEntry.grid(row=5, column=1, padx=5, pady=5)
+
+    # Languages spoken by refugees
+    languages_spokenlabel = tk.Label(refugee_labelframe, text='Please select main language spoken in the family',
+                                     font=('TkinterDefault', 15))
+    languages_spokenlabel.grid(row=6, column=0, padx=5, pady=5)
+    t_languages_spokenEntry = ttk.Combobox(refugee_labelframe,
+                                           values='English Chinese Hindi Spanish French Arabic Bengali Portuguese Russian Urdu Indonesian German Swahili Marathi Tamil Telugu Turkish Vietnamese Korean Italian Thai Gujarati Persian Polish Pashto Kannada Ukrainian Somali Kurdish')
+    t_languages_spokenEntry.grid(row=6, column=1, padx=5, pady=5)
+
+    # secondlanguage entry box
+    second_languagelabel = tk.Label(refugee_labelframe,
+                                    text='Please enter any other languages spoken by each family member. If none, please enter \'none\'',
+                                    font=('TkinterDefault', 15))
+    second_languagelabel.grid(row=7, column=0, padx=5, pady=5)
+    t_second_languageEntry = tk.Entry(refugee_labelframe)
+    t_second_languageEntry.grid(row=7, column=1, padx=5, pady=5)
+
+    na_store_details = tk.Button(refugee_labelframe, text="Store refugee info", command=lambda: [
+        store_details_callback(t_camp_IDbox, family_labelbox, t_medical_conditionsEntry, t_languages_spokenEntry,
+                               t_second_languageEntry, name_entry), back_button_to_volunteer_main], height=1, width=20)
+    na_store_details.grid(row=8, column=1, pady=5)
+
+    # Back button
+    t_back_button = tk.Button(refugee_labelframe, text='Back to Home', command=back_button_to_volunteer_main)
+    t_back_button.grid(row=9, column=0, padx=5, pady=10)
+
+    for i in range(10):
+        refugeeframe.grid_rowconfigure(i, weight=1)
+    refugeeframe.grid_columnconfigure(0, weight=1)
+
+    return t_camp_IDbox, name_entry, t_medical_conditionsEntry, t_languages_spokenEntry, t_second_languageEntry
+
+
+def na_refugee_info_dict(refugee_info, t_camp_IDbox, family_labelbox, t_medical_conditionsEntry, t_languages_spokenEntry,
+                               t_second_languageEntry, name_entry):
+    try:
+        # Update self.t_create_refugee dictionary
+        refugee_info = pd.read_csv('refugee_info.csv', index_col='Name')
+
+
+        camp_ID = int(t_camp_IDbox.get())
+        name = name_entry.get()
+        family_members = family_labelbox.get()
+        medical_conditions = t_medical_conditionsEntry.get()
+        languages_spoken = t_languages_spokenEntry.get()
+        second_language = t_second_languageEntry.get()
+        refugee_info.loc[name,'Camp ID'] = camp_ID
+        camp_ID = int(camp_ID)
+        refugee_info.loc[name,'Medical Conditions'] = medical_conditions
+        refugee_info.loc[name, 'Languages Spoken'] = languages_spoken
+        refugee_info.loc[name, 'Second Language'] = second_language
+        refugee_info.loc[name, 'Family Members'] = family_members
+
+        # new_refugee_info.index.name = 'Name'
+        refugee_info.to_csv('refugee_info.csv')
+        update_number_of_refugees(camp_ID)
+        tk.messagebox.showinfo(title='Details saved', message='Details have been saved')
+
+    except FileNotFoundError:
+        data = {'Name': [name], 'Camp ID': [camp_ID],
+                'Medical Conditions': [medical_conditions],
+                'Languages Spoken': [languages_spoken],
+                'Second Language': [second_language],
+                'Family Members': [family_members]}
+        refugee_info = pd.DataFrame(data)
+        refugee_info.set_index('Name', inplace=True)
+        refugee_info.to_csv('refugee_info.csv')
+
+def update_number_of_refugees(camp_ID):
+    number_of_refugees_actual = 0
+    with open('refugee_info.csv', 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            if camp_ID == int(float(row[1])):
+                number_of_refugees_actual += 1
+    print(f"Total refugees counted: {number_of_refugees_actual}")
+
+    header = []
+    data = []
+    with open('crisis_events.csv', 'r') as file:
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+        data = list(csv_reader)
+
+    for row in data:
+        if int(float(row[0])) == int(camp_ID):
+            row[16] = str(number_of_refugees_actual)
+
+    with open('crisis_events.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(data)
---
+++ volunteer login code from prerevert.py
@@ -0,0 +1,196 @@
+import tkinter as tk
+from tkinter import messagebox
+import os
+import pickle
+import pandas as pd
+import csv
+volunteer_camp_id_for_new_refugee = None
+
+class t_case_sensitive(Exception):
+    pass
+class t_no_text(Exception):
+    pass
+class t_incorrect_details(Exception):
+    pass
+class t_deleted_account(Exception):
+    pass
+class t_deactivated_account(Exception):
+    pass
+
+class VolunteerLoginPage(tk.Frame, t_deactivated_account, t_deleted_account, t_case_sensitive, t_no_text, t_incorrect_details):
+    def __init__(self, root, window, go_to_volunteer_main, open_admin_login, open_volunteer_login, exit_software):
+        super().__init__(root)
+        self.root = root
+        self.window = window
+        self.go_to_volunteer_main = go_to_volunteer_main
+        self.open_admin_login = open_admin_login
+        self.open_volunteer_login = open_volunteer_login
+        self.exit_software = exit_software
+
+        try:
+            self.y_personal_info = pd.read_csv('volunteer_info.csv', index_col='Username')
+            self.y_personal_info = self.y_personal_info.to_dict(orient='index')
+            print(self.y_personal_info)
+
+        except(FileNotFoundError):
+            self.y_personal_info = {
+                'volunteer1': {'password': '111', 'name': '', 'Email Address': '', 'Phone Number': '', 'Commitment': '',
+                               'Work Type': '', 'Deactivated': False, 'Deleted': False},
+                'volunteer2': {'password': '111', 'name': '', 'Email Address': '', 'Phone Number': '', 'Commitment': '',
+                               'Work Type': '', 'Deactivated': False, 'Deleted': False},
+                'volunteer3': {'password': '111', 'name': '', 'Email Address': '', 'Phone Number': '', 'Commitment': '',
+                               'Work Type': '', 'Deactivated': True, 'Deleted': False},
+                'volunteer4': {'password': '111', 'name': '', 'Email Address': '', 'Phone Number': '', 'Commitment': '',
+                               'Work Type': '', 'Deactivated': False, 'Deleted': False}
+            }
+
+            df = pd.DataFrame.from_dict(self.y_personal_info, orient='index')
+            df.index.name = 'Username'
+            df.to_csv('volunteer_info.csv', index='Username')
+            self.y_personal_info = pd.read_csv('volunteer_info.csv')
+
+        #self.y_camp_info = {"Syria": {"ID": "123098", "Max Capacity": ""}}
+
+        welcome_label = tk.Label(
+            self,
+            text='UCL Humanity Rescue Volunteer Portal',
+            font=('TkDefaultFont', 25, 'bold'),
+            foreground='blue', relief=tk.RAISED, borderwidth=5
+        )
+        welcome_label.grid(row=1, column=0, padx=30, pady=30)
+
+        # instruction_label = tk.Label(
+        #     self,
+        #     text='Volunteer login',
+        #     font=('TkDefaultFont', 20)
+        # )
+        # instruction_label.grid(row=1, column=0, pady=30)
+
+        volunteer_entries_frame = tk.Frame(self, relief=tk.RAISED, borderwidth=5)
+        volunteer_entries_frame.grid()
+
+        # Label frame for this page that then stores all of the labels and entries
+        volunteer_log_in_frame = tk.LabelFrame(volunteer_entries_frame, borderwidth=5)
+        volunteer_log_in_frame.grid(row=3, column=1, pady=30, padx=30)
+
+        # Volunteer title
+        #self.t_volunteer_title = tk.Label(self, text='Volunteer login', font=('Arial bold', 50))
+        #self.t_volunteer_title.grid(row=0, column=0, pady=30)
+
+        # Username entry box
+        self.name_label = tk.Label(volunteer_log_in_frame, text='Username', font=('TkDefault', 17))
+        self.name_label.grid(row=6, column=0, pady=10, padx=10)
+        self.name_entry = tk.Entry(volunteer_log_in_frame)
+        self.name_entry.grid(row=6, column=1, pady=10, padx=10)
+
+        # Password entry box
+        self.password_label2 = tk.Label(volunteer_log_in_frame, text='Password', font=('TkDefault', 17))
+        self.password_label2.grid(row=8, column=0, pady=10)
+        self.password_entry = tk.Entry(volunteer_log_in_frame, show='*')
+        self.password_entry.grid(row=8, column=1, pady=10)
+        self.password_entry.bind("<KeyPress>", self.caps_lock_on)
+        self.password_entry.bind("<KeyRelease>", self.caps_lock_off)
+
+        # # Username entry box
+        # self.t_name_label = tk.Label(self, text='Username')
+        # self.t_name_label.grid(row=1, column=0)
+        # self.t_name_entry = tk.Entry(self)
+        # self.t_name_entry.grid(row=2, column=0)
+        #
+        # # Password entry box
+        # self.t_password_label2 = tk.Label(self, text='Password')
+        # self.t_password_label2.grid(row=3, column=0, pady=5)
+        # self.t_password_entry = tk.Entry(self, show='*')
+        # self.t_password_entry.grid(row=4, column=0)
+
+        # Login box
+        # self.entry_button = tk.Button(self, text='Login', command=self.t_details_confirmation, height=1, width=20)
+        # self.entry_button.grid(row=5, column=0, pady=20)
+        #
+        # # Back to landing page
+        # login_btn = tk.Button(self.window, text="Back", command=self.exit_and_go_back)
+        # login_btn.grid(row=10, column=0, pady=10)
+
+
+        login_btn = tk.Button(volunteer_log_in_frame, text="Login", command=self.t_details_confirmation)
+        login_btn.grid(row=10, column=1, pady=10)
+        # Swap button from command=self.t_details_confirmation to command=self.on_login to bypass
+
+        # Back to landing page
+        back_button = tk.Button(volunteer_log_in_frame, text="Back", command=self.exit_and_go_back)
+        back_button.grid(row=10, column=0, pady=10)
+
+
+        # Shows caps lock on/off (OLD BINDING NOT WORKING)
+        self.label_caps = tk.Label(volunteer_log_in_frame, text='')
+        self.label_caps.grid(row=4, column=1)
+        #self.bind("<KeyPress>", self.caps_lock_on)
+        #self.bind("<KeyRelease>", self.caps_lock_off)
+        for i in range(11):
+            self.grid_rowconfigure(i, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+    # For caps lock on/off
+    def caps_lock_on(self, event):
+        if event.keysym == 'Caps_Lock':
+            self.label_caps.config(text='Caps lock is on')
+
+    def caps_lock_off(self, event):
+        if event.keysym == 'Caps_Lock':
+            self.label_caps.config(text='Caps lock is off')
+
+    def get_camp_id_for_volunteer_for_new_refugee(self):
+        global volunteer_camp_id_for_new_refugee
+
+        with open("volunteer_info.csv", 'r') as file:
+            csv_reader = csv.reader(file)
+
+            # Name is in the first column (index 0)
+            for row in csv_reader:
+                if row and str(row[0].strip()) == self.username:
+                    # Camp ID is the second value (index 1)
+                    volunteer_camp_id_for_new_refugee = row[1].strip()
+                    print(volunteer_camp_id_for_new_refugee)
+                    break
+        return volunteer_camp_id_for_new_refugee
+
+    # Login checker
+    def t_details_confirmation(self):
+        self.username = self.name_entry.get()
+        print(self.name_entry.get())
+        password = self.password_entry.get()
+        try:
+            if self.username not in self.y_personal_info.keys():
+                raise t_deleted_account
+            elif self.y_personal_info[self.username]['Deactivated'] == True:
+                raise t_deactivated_account
+            else:
+                if password == str(self.y_personal_info[self.username]['password']):
+                    self.go_to_volunteer_main(self.username)
+                    self.get_camp_id_for_volunteer_for_new_refugee()
+                elif password != self.y_personal_info[self.username]['password']:
+                    raise t_incorrect_details
+
+            # if (username.lower() or password.lower()) in self.volunteer_dict:
+            # raise t_case_sensitive
+        except(t_incorrect_details):
+            tk.messagebox.showinfo(title='Incorrect details', message='The password does not match the username')
+        # except(t_case_sensitive):
+        # tkinter.messagebox.showinfo(title='Incorrect capitals', message='Check to make sure you didn\'t accidentally use caps lock')
+        except(t_deleted_account):
+            tk.messagebox.showinfo(title='Username non-existent', message='That is not a valid volunteer username')
+        except(t_deactivated_account):
+            tk.messagebox.showinfo(title='Deactivated account',
+                                        message='Your account has been deactivated, \nPlease contact admin for more details')
+
+    def on_login(self):
+        # Add volunteer login logic here
+        username = ""
+        self.go_to_volunteer_main(username)
+
+    def exit_and_go_back(self):
+        for widget in self.window.winfo_children():
+            widget.destroy()
+        from landing_page import LandingPage
+        LandingPage.create_landing_page_widgets(self)
+
